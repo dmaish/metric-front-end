1bfd40df0df6eb36b8c022d0af11bf196d3e810b
dmaish <mainadaniel81@gmail.com>
2019-11-04 14:53:13 +0300

    register div styling complete

--- src/views/auth/index.js
+++ src/views/auth/index.js
@@ -16,7 +16,42 @@ class Auth extends Component {
     render() {
         return (
                 <div id="auth" style={styles.modalBackground} className="auth-modal">
-                    <div className="register-div">
+                    <div className="auth-div">
+                        <div className="register-div">
+                            <div className="title">
+                                REGISTER
+                            </div>
+                            <div className="tagline">
+                                Already have an account? Login here.
+                            </div>
+                            <div className="username input-container">
+                                <input placeholder="username"/>
+                                </div>
+                            <div className="email input-container">
+                                <input placeholder="email"/>
+                            </div>
+                            <div className="password input-container">
+                                <input placeholder="password"/>
+                            </div>
+                            <div className="confirm-pass input-container">
+                                <input placeholder="confirm password"/>
+                            </div>
+                            <div className="input-container">
+                                <button className="auth-button">
+                                    create account
+                                </button>
+                            </div>
+                            <div className="input-container">
+                                <div className="horizontal-line"><hr/></div>
+                                <div style={{display: 'inline-block', fontSize: '17px', width: '10%', color: '#ffffff'}}>or</div>
+                                <div className="horizontal-line"><hr/></div>
+                            </div>
+                            <div className="input-container">
+                                <button className="google-button">
+                                    GOOGLE
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
         );
